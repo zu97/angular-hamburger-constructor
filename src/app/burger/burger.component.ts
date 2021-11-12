bf53b7fd26200fb02cca870f8672cceedbabbf94
admin <admin@admin.com>
2021-11-12 17:32:19 +0600

    Visual display of ingredients in a burger

--- src/app/burger/burger.component.ts
+++ src/app/burger/burger.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Ingredient} from "../shared/ingredient.model";
 
 @Component({
   selector: 'app-burger',
@@ -6,5 +7,15 @@ import {Component} from '@angular/core';
   styleUrls: ['./burger.component.css']
 })
 export class BurgerComponent {
+  @Input() ingredients!: Ingredient[];
 
+  getIngredients() {
+    return this.ingredients.reduce((acc: string[], ingredient) => {
+      for (let i = 0; i < ingredient.count; i++) {
+        acc.push(ingredient.name);
+      }
+
+      return acc;
+    }, []).reverse();
+  }
 }
